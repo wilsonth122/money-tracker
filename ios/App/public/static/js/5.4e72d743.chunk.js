d2d8c18f940a3febd61fd0189010a73ca868cabe
Thomas Wilson <wilsonth122@gmail.com>
2019-09-11 10:28:07 +0100

    Andorid and IOS deployment files

---
+++ ios/App/public/static/js/5.4e72d743.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{636:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",function(){return i});var e=o(74),i=(o(15),function(){var n=window;n.addEventListener("statusTap",function(){Object(e.g)(function(){var t=n.innerWidth,o=n.innerHeight,i=document.elementFromPoint(t/2,o/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then(function(){Object(e.n)(function(){return c.scrollToTop(300)})})}})})})}}]);
+//# sourceMappingURL=5.4e72d743.chunk.js.map
